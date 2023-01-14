d559fa372523e67ad1e529d326fa733ab88defd4
Lucie <lucie.fischer@outlook.com>
2023-01-14 18:25:43 +0100

    oauth2 authent

--- src/main/java/com/poseidon/poseidon/configuration/SpringSecurityConfig.java
+++ src/main/java/com/poseidon/poseidon/configuration/SpringSecurityConfig.java
@@ -31,19 +31,21 @@ public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable).authorizeRequests(auth -> {
-            auth.antMatchers("/").permitAll();
-            auth.antMatchers("/admin/**").hasRole("ADMIN");
-            auth.antMatchers("/user/**").hasRole("ADMIN");
-            auth.anyRequest().authenticated();
-        }).formLogin(formLogin -> {
-            formLogin.loginPage("/login");
-            formLogin.loginProcessingUrl("/login");
-            formLogin.defaultSuccessUrl("/");
-            formLogin.permitAll();
-        }).logout(logout -> {
-            logout.logoutUrl("/app-logout");
-            logout.deleteCookies("JSESSIONID");
-        }).build();
+                    auth.antMatchers("/").permitAll();
+                    auth.antMatchers("/admin/**").hasRole("ADMIN");
+                    auth.antMatchers("/user/**").hasRole("ADMIN");
+                    auth.anyRequest().authenticated();
+                }).oauth2Login(oauth2Login -> {
+                    oauth2Login.loginPage("/login");
+                   // oauth2Login.loginProcessingUrl("/login");
+                   // oauth2Login.defaultSuccessUrl("/");
+                    oauth2Login.permitAll();
+                }).logout(logout -> {
+                    logout.logoutUrl("/app-logout");
+                    logout.deleteCookies("JSESSIONID");
+                })
+              //  .oauth2Login(oauth2Login -> {oauth2Login.loginPage("/login");})
+                .build();
     }
 
 }
--- src/main/java/com/poseidon/poseidon/controllers/LoginController.java
+++ src/main/java/com/poseidon/poseidon/controllers/LoginController.java
@@ -2,11 +2,18 @@ package com.poseidon.poseidon.controllers;
 
 import com.poseidon.poseidon.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ResolvableType;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class LoginController {
@@ -15,7 +22,9 @@ public class LoginController {
     private UserRepository userRepository;
 
     @GetMapping("/login")
-    public String login() {
+    public String login(Model model) {
+        String gitHubUrl =  "oauth2/authorization/github" ;
+        model.addAttribute("gitHubUrl", gitHubUrl);
         return "login";
     }
 
